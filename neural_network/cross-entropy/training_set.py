2ab416bbe08a746c2c43b1f11df584eb564694aa
ConnorUllmann <connorullmann@Connors-MacBook-Pro.local>
2016-10-20 02:13:50 -0400

    Refactoring and cleaning up

--- neural_network/cross-entropy/training_set.py
+++ neural_network/cross-entropy/training_set.py
@@ -1,65 +1,28 @@
 from numpy import array
 from random import shuffle
-
-# ----------- TEST FUNCTIONS --------------
-def xor(*args):
-    x = args[0]
-    for i in range(1, len(args)):
-        x = (x != args[i])
-    return [int(x)]
-
-def example(*args):
-    return [int(args[0] != args[1])]
-
-def add(*args):
-    mid = int(len(args)/2)
-    a = 0
-    b = 0
-    c = 0
-    for i in range(0, int(mid)):
-        a += pow(2, i) * args[len(args)-(i+mid)-1]
-        b += pow(2, i) * args[len(args)-i-1]
-        c += pow(2, i)
-    _y = int(a+b)
-    _yString = "{0:b}".format(_y)
-    _length = len("{0:b}".format(int(c*2+1)))
-    ret = []
-    for i in range(len(_yString)):
-        __y = _yString[i]
-        ret.append(int(__y == '1'))
-    while len(ret) < _length:
-        ret.insert(0, 0)
-    #print("{} + {} = {}".format(a, b, _y))
-    #print("{} + {} = {}".format(args[:mid], args[mid:], ret))
-    return ret
-# -----------------------------------------
+from utils import *
 
 class TrainingSet:
-    def __init__(self, bitLength, function):        
+    def __init__(self, bitLengthInput, bitLengthOutput, function, testProportion=0.1):        
         inputBinarySet = []
         outputBinarySet = []
-        xMax = int(pow(2, bitLength))
+
+        xMax = int(pow(2, bitLengthInput))
         for x in range(0, xMax):
-            inputBinary = "{0:b}".format(x)
-            while len(inputBinary) < bitLength:
-                inputBinary = '0' + inputBinary
-            inputBinaryList = []
-            for character in inputBinary:
-                inputBinaryList.append(int(character))
+            inputBinaryList = Binary(x, bitLengthInput, True)
             inputBinarySet.append(inputBinaryList)
-            
-            y = function(*inputBinaryList)
+            y = Binary(function(*inputBinaryList), bitLengthOutput, True)
             outputBinarySet.append(y)
         
-        self.tests = []
+        tests = []
         for i, o in zip(array(inputBinarySet), array(outputBinarySet)):
-            self.tests.append((i, o))
-        shuffle(self.tests)
+            tests.append((i, o))
 
-    def PluckTestSet(self, n=0.1):
-        if len(self.tests) <= 0:
-            return []
-        i = max(1, int(len(self.tests)*n))
-        testSet = self.tests[:i]
-        self.tests = self.tests[i:]
-        return testSet
+        if len(tests) <= 0:
+            self.exams = []
+            self.tests = []
+        else:
+            shuffle(tests)
+            i = max(1, int(len(tests) * testProportion))
+            self.exams = tests[:i]
+            self.tests = tests[i:]
--- neural_network/cross-entropy/utils.py
+++ neural_network/cross-entropy/utils.py
@@ -1,5 +1,9 @@
 from math import *
 from numpy import exp
+from datetime import timedelta
+
+def RemoveMilliseconds(delta):
+    return delta - timedelta(microseconds=delta.microseconds)
 
 def Spiral(n):
     if n==0:
@@ -37,14 +41,14 @@ def Heartbeat(x):
 # We pass the weighted sum of the inputs through this function to
 # normalise them between 0 and 1.
 def Sigmoid(x):
-    return 1 / (1 + exp(-x))
+    return 1.0 / (1.0 + exp(-x))
         
 
 # The derivative of the Sigmoid function.
 # This is the gradient of the Sigmoid curve.
 # It indicates how confident we are about the existing weight.
 def SigmoidDerivative(x):
-    return x * (1 - x)
+    return Sigmoid(x) * (1.0 - Sigmoid(x))
 
 def Add(a, b):
     return [a[0] + b[0], a[1] + b[1]]
@@ -96,3 +100,56 @@ def TorquePushVector(centerOfMass, forcePosition, forceVector):
 def TorquePullVector(centerOfMass, forcePosition, forceVector):
     r = (forcePosition[1] - centerOfMass[1], -forcePosition[0] + centerOfMass[0])
     return Proj(forceVector, r)
+
+def Binary(x, digits=-1, asList=False):
+    if type(x) is list:
+        s = ""
+        for c in x:
+            s = "{}{}".format(s, c)
+    else:
+        s = "{0:b}".format(int(x))
+    
+    while len(s) < digits:
+        s = "0" + s
+    if digits > 0:
+        s = s[-digits:]
+
+    if asList:
+        r = []
+        for c in s:
+            r.append(int(c))
+        return r
+    return s
+
+def Xor(*args):
+    x = args[0]
+    for i in range(1, len(args)):
+        x = (x != args[i])
+    return [int(x)]
+
+def Opp(*args):
+    return [int(args[0] != args[1])]
+
+def Add(*args):
+    mid = int(len(args)/2)
+    a = 0
+    b = 0
+    c = 0
+    for i in range(0, int(mid)):
+        power = pow(2, i)
+        k = len(args) - 1 - i
+        a += power * args[k - mid]
+        b += power * args[k]
+        c += power
+    c = c * 2 + 1
+    y = int(a + b)
+    yString = Binary(y)
+    
+    ret = []
+    for c in yString:
+        ret.append(int(c == '1'))
+    while len(ret) < len(Binary(c)):
+        ret.insert(0, 0)
+    #print("{} + {} = {}".format(a, b, y))
+    #print("{} + {} = {}".format(args[:mid], args[mid:], ret))
+    return ret
