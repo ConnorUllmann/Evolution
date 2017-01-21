7e6cb590a85b0de6f0906ad51426ac61c35e0017
ConnorUllmann <connorullmann@Connors-MacBook-Pro.local>
2017-01-21 14:09:27 -0500

    Entity class added and cleaning up project

---
+++ arena/entity.py
@@ -0,0 +1,58 @@
+from screen import Screen
+from state_machine import StateMachine
+from utils import Point
+
+class Entity(StateMachine, Point):
+
+    ID = 0
+    entities = {}
+
+    @staticmethod
+    def GetId():
+        _id = Entity.ID
+        Entity.ID += 1
+        return _id
+
+    @staticmethod
+    def Add(entity):
+        name = entity.__class__.__name__
+        if name not in Entity.entities:
+            Entity.entities[name] = []
+        Entity.entities[name].append(entity)
+
+    @staticmethod
+    def Remove(entity):
+        name = entity.__class__.__name__
+        if name in Entity.entities and entity in Entity.entities[name]:
+            Entity.entities[name].remove(entity)
+            if len(Entity.entities[name]) <= 0:
+                Entity.entities.pop(name, None)
+            return True
+        return False
+
+    def __init__(self, x=0, y=0):
+        Point.__init__(self, x, y)
+        StateMachine.__init__(self)
+        
+        self.id = Entity.GetId()
+        Entity.Add(self)
+        
+        Screen.Instance.AddUpdateFunction(self, self.Update)
+        Screen.Instance.AddRenderFunction(self, self.Render)
+        Screen.PutOnTop(self)
+        
+        self.v = Point()
+        self.destroyed = False
+
+    def Render(self):
+        pass
+
+    def __str__(self):
+        return "[{}]{}".format(self.id, self.__class__.__name__)
+    
+    def Destroy(self):
+        if not self.destroyed:
+            self.destroyed = True
+            Screen.Instance.RemoveUpdateFunctions(self)
+            Screen.Instance.RemoveRenderFunctions(self)
+            Entity.Remove(self)
--- arena/main.py
+++ arena/main.py
@@ -1,60 +1,17 @@
 from screen import Screen
 from player import Player
-from wall import Wall
 import pygame, random, math
-from utils import Point
-
-wallWidth = 32
-gap = 30
-gapY = 200
-screenSpeed = 6
-camera = Point(0, 0)
-wallMaxX = 0
 
 def PreGame():
-    global player, wallWidth
-    pygame.display.set_caption("Helicopter")
-    player = Player(200, gapY)
-    player.v.x = screenSpeed
-    pass
+    pygame.display.set_caption("Arena")
+    Player(200, 200)
+    Player(400, 300)
 
 def UpdateGame():
-    global player, camera, wallMaxX, wallWidth, gap, gapY, screenSpeed
-    cameraLeftMargin = 200
-    newWallMaxX = math.floor((player.x + Screen.Instance.width + wallWidth - cameraLeftMargin) / wallWidth) * wallWidth
-    if newWallMaxX > wallMaxX:
-        for x in range(wallMaxX, newWallMaxX, wallWidth):
-            gapY += random.randint(-30, 30)
-            gap += random.randint(-5, 5)
-
-            gap = max(min(gap, 80), 40)
-            gapY = max(min(gapY, Screen.Instance.height-gap-10), gap+10)
-            
-            Wall(x, 0, wallWidth, gapY - gap)
-            Wall(x, gapY + gap, wallWidth, Screen.Instance.height - gapY - gap)
-            
-        wallMaxX = newWallMaxX
-
-    Screen.Instance.camera.x = player.x - cameraLeftMargin
-    Screen.Instance.camera.y = 0
-
-    for event in pygame.event.get():
-        if (event.type == pygame.KEYDOWN and event.key == pygame.K_b):
-            player.y = gapY
-            player.v.y = 0
     pygame.event.poll()
-    
-    walls = []
-    for wall in Wall.walls:
-        walls.append(wall)
-    for wall in walls:
-        if wall.x - Screen.Instance.camera.x < -wallWidth:
-            wall.Destroy()
     pass
 
 def RenderGame():
-    global player, maxX, wallWidth
-    pygame.display.set_caption("Helicopter")
     pass
     
 def StartGame():
