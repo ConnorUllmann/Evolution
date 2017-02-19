91ff71cde1ffde1ed83546a3754578271e2fbf89
ConnorUllmann <connorullmann@Connors-MacBook-Pro.local>
2017-02-19 15:09:04 -0500

    Polygon intersection algorithm now generates a minimal set of intersecting polygons
    Lots of logging added and the code is ready to be optimized. Also
    created a random polygon generator

--- arena/basics/polygon.py
+++ arena/basics/polygon.py
@@ -55,10 +55,16 @@ class PolygonEntity(Polygon, Entity):
             allIntersectionPoints.extend(intersectionPoints)
         return allIntersectionPoints
 
-    def Traverse(self, other):
-        return self.TraverseHandoff(other, 0, self.VerticesWithIntersectionPoints(other), other.VerticesWithIntersectionPoints(self), [[]], None, Drawable(self.vertices, self), Drawable(other.vertices, other))
+    def Traverse(self, other, verticesA, verticesB):
+        polygonPoints = self.TraverseHandoff(other, 0, verticesA, verticesB, [[]], None, Drawable(self.vertices, self)[:-1], Drawable(other.vertices, other)[:-1], [])
+        if len(polygonPoints) > 0 and len(polygonPoints[-1]) <= 0:
+            polygonPoints = polygonPoints[:-1]
+        return polygonPoints
 
-    def TraverseHandoff(self, other, index, verticesA, verticesB, polygonPoints, nowInside, uncheckedA, uncheckedB):
+    def TraverseHandoff(self, other, index, verticesA, verticesB, polygonPoints, nowInside, uncheckedA, uncheckedB, addedIntersections):
+        debug = False
+        nameA = "A" if self.color==(128, 255, 128) else "B"
+        nameB = "B" if nameA == "A" else "A"
         i = index
         polygon = polygonPoints[-1]
         while True:
@@ -66,49 +72,112 @@ class PolygonEntity(Polygon, Entity):
             b = verticesA[(i+1)%len(verticesA)]
             c = (a + b)/2
 
+            if debug:
+                print("{}{}".format(nameA, i%len(verticesA)))
+
             if a in uncheckedA:
+                if debug:
+                    print("{}{} removed from unchecked{} index:{}".format(nameA, i%len(verticesA), nameA, uncheckedA.index(a)))
                 uncheckedA.remove(a)
             if a in uncheckedB:
+                if debug:
+                    print("{}{} removed from unchecked{} index:{}".format(nameA, i%len(verticesA), nameB, uncheckedB.index(a)))
                 uncheckedB.remove(a)
 
             if nowInside is None:
-                nowInside = other.contains(a)
-                if nowInside:
+                if other.contains(a):
+                    if debug:
+                        print("added {}{} to polygon {}".format(nameA, i%len(verticesA), polygonPoints.index(polygon)))
                     polygon.append(a)
+                nowInside = other.contains(c)
             else:
                 finishedPolygon = len(polygon) > 0 and a == polygon[0]
                 if finishedPolygon:
+                    if debug:
+                        print("(new polygon) unchecked ([{}]{} [{}]{})".format(len(uncheckedA), nameA, len(uncheckedB), nameB))
+                        for ptA in uncheckedA:
+                            print(" {}{}".format(nameA, verticesA.index(ptA)))
+                        print(" --- ")
+                        for ptB in uncheckedB:
+                            print(" {}{}".format(nameB, verticesB.index(ptB)))
                     if len(uncheckedA) > 0:
-                        return self.TraverseHandoff(other, verticesA.index(uncheckedA.pop(0)), verticesA, verticesB, polygonPoints+[[]], None, uncheckedA, uncheckedB)
+                        return self.TraverseHandoff(other, verticesA.index(uncheckedA[0]), verticesA, verticesB, polygonPoints+[[]], None, uncheckedA, uncheckedB, addedIntersections)
                     if len(uncheckedB) > 0:
-                        return other.TraverseHandoff(self, verticesB.index(uncheckedB.pop(0)), verticesB, verticesA, polygonPoints+[[]], None, uncheckedB, uncheckedA)
+                        return other.TraverseHandoff(self, verticesB.index(uncheckedB[0]), verticesB, verticesA, polygonPoints+[[]], None, uncheckedB, uncheckedA, addedIntersections)
                     return polygonPoints
 
                 wasInside = nowInside
                 nowInside = other.contains(c)
                 if nowInside:
+                    if debug:
+                        print("added {}{} to polygon {}".format(nameA, i%len(verticesA), polygonPoints.index(polygon)))
+                    if a in addedIntersections:
+                        if debug:
+                            print("unchecked ([{}]{} [{}]{})".format(len(uncheckedA), nameA, len(uncheckedB), nameB))
+                            for ptA in uncheckedA:
+                                print(" {}{}".format(nameA, verticesA.index(ptA)))
+                            print(" --- ")
+                            for ptB in uncheckedB:
+                                print(" {}{}".format(nameB, verticesB.index(ptB)))
+                        if len(uncheckedA) > 0:
+                            return self.TraverseHandoff(other, verticesA.index(uncheckedA[0]), verticesA, verticesB, polygonPoints, None, uncheckedA, uncheckedB, addedIntersections)
+                        if len(uncheckedB) > 0:
+                            return other.TraverseHandoff(self, verticesB.index(uncheckedB[0]), verticesB, verticesA, polygonPoints, None, uncheckedB, uncheckedA, addedIntersections)
+                        return polygonPoints
+                    elif a in verticesA and a in verticesB:
+                        addedIntersections.append(a)
                     polygon.append(a)
                 elif wasInside:
                     if a in verticesB:
-                        return other.TraverseHandoff(self, verticesB.index(a), verticesB, verticesA, polygonPoints, False, uncheckedB, uncheckedA)
+                        if debug:
+                            print("handoff to other")
+                        return other.TraverseHandoff(self, verticesB.index(a), verticesB, verticesA, polygonPoints, nowInside, uncheckedB, uncheckedA, addedIntersections)
+                elif len(polygon) <= 0:
+                    if debug:
+                        print("unchecked ([{}]{} [{}]{})".format(len(uncheckedA), nameA, len(uncheckedB), nameB))
+                        for ptA in uncheckedA:
+                            print(" {}{}".format(nameA, verticesA.index(ptA)))
+                        print(" --- ")
+                        for ptB in uncheckedB:
+                            print(" {}{}".format(nameB, verticesB.index(ptB)))
+                    if len(uncheckedA) > 0:
+                        return self.TraverseHandoff(other, verticesA.index(uncheckedA[0]), verticesA, verticesB, polygonPoints, None, uncheckedA, uncheckedB, addedIntersections)
+                    if len(uncheckedB) > 0:
+                        return other.TraverseHandoff(self, verticesB.index(uncheckedB[0]), verticesB, verticesA, polygonPoints, None, uncheckedB, uncheckedA, addedIntersections)
+                    return polygonPoints
+
             i += 1
 
     def IntersectionPolygon(self, other, color):
+        verticesA = self.VerticesWithIntersectionPoints(other)
+        verticesB = other.VerticesWithIntersectionPoints(self)
 
-        polygonsPoints = self.Traverse(other)
+        # print("verticesA:")
+        # for i in range(len(verticesA)):
+        #     print("A{} = {}".format(i, verticesA[i]))
+        # print("verticesB:")
+        # for i in range(len(verticesB)):
+        #     print("B{} = {}".format(i, verticesB[i]))
+
+        polygonsPoints = self.Traverse(other, verticesA, verticesB)
 
         s = ""
-        for polygonPoints in polygonsPoints:
-            s += "points[{}]: ".format(len(polygonPoints))
-            for pt in polygonPoints:
-                s += "(" + str(int(pt.x)) + ", " + str(int(pt.y)) + ")"
-            s += "\n"
-        print("intersectionPolygon[{}] poly points={}".format(len(polygonPoints), s))
-
-        print(len(polygonsPoints))
+        # for polygonPoints in polygonsPoints:
+        #     s += "points[{}]:\n".format(len(polygonPoints))
+        #     for pt in polygonPoints:
+        #         inA = pt in verticesA
+        #         inB = pt in verticesB
+        #
+        #         if inA and inB:
+        #             s += " A{} == B{}\n".format(verticesA.index(pt), verticesB.index(pt))#"(" + str(int(pt.x)) + ", " + str(int(pt.y)) + ")"
+        #         elif inA:
+        #             s += " A{}\n".format(verticesA.index(pt))
+        #         elif inB:
+        #             s += " B{}\n".format(verticesB.index(pt))
+        print("Number of polygons: {}\n{}---".format(len(polygonsPoints), s))
         for polygonVertices in polygonsPoints:
             if len(polygonVertices) > 1:
-                Screen.DrawLines(Drawable(polygonVertices), color, 4)
+                Screen.DrawLines(Drawable(polygonVertices), color, 2)
 
     def __init__(self, x, y, vertices=[], color=(255, 255, 255), thickness=1):
         Polygon.__init__(self, vertices)
@@ -122,4 +191,4 @@ class PolygonEntity(Polygon, Entity):
             Screen.DrawLines(Drawable(self.vertices, self), self.color, self.thickness)
         for i in range(len(self.vertices)):
             vertex = self.vertices[i]
-            Screen.DrawCircle(self + vertex, 2+i, (255, 255, 255))
+            Screen.DrawCircle(self + vertex, 2, (255, 255, 255))
--- arena/main_polygon_test.py
+++ arena/main_polygon_test.py
@@ -1,6 +1,8 @@
 from basics import Screen, PolygonEntity, Point
 import pygame
 from time import time
+from math import pi
+from random import random
 
 
 def PreGame():
@@ -10,47 +12,78 @@ A = None
 B = None
 def BeginGame():
     global A, B
-    verticesA = [
-        Point(300, 400, True),
-        Point(100, 100, True),
-        Point(380, 300, True)
-    ]
+    # verticesA = [
+    #     # Point(100, 100, True),
+    #     # Point(300, 100, True),
+    #     # Point(200, 300, True)
+    #     Point(300, 400, True),
+    #     Point(100, 100, True),
+    #     Point(380, 300, True)
+    # ]
+    # verticesB = [
+    #     # Point(200, 50, True),
+    #     # Point(300, 400, True),
+    #     # Point(100, 400, True)
+    #     Point(400, 300, True),
+    #     Point(240, 380, True),
+    #     Point(300, 280, True),
+    #     Point(140, 280, True),
+    #     Point(140, 260, True),
+    #     Point(200, 200, True),
+    #     Point(220, 100, True)
+    # ]
 
-    verticesB = [
-        Point(400, 300, True),
-        Point(240, 380, True),
-        Point(300, 280, True),
-        Point(140, 280, True),
-        Point(140, 260, True),
-        Point(200, 200, True),
-        Point(220, 100, True)
-    ]
+    verticesA = []
+    angle = 0
+    while angle < 2 * pi:
+        length = random() * 250 + 50
+        angle += random() * 0.5
+        point = Point(length, 0)
+        point.radians = angle
+        verticesA.append(point)
 
-    A = PolygonEntity(50, 20, verticesA, (128, 255, 128))
-    B = PolygonEntity(50, 20, verticesB, (128, 128, 255))
+    verticesB = []
+    angle = 0
+    while angle < 2 * pi:
+        length = random() * 290 + 10
+        angle += random() * 0.5
+        point = Point(length, 0)
+        point.radians = angle
+        verticesB.append(point)
+
+    A = PolygonEntity(400, 400, verticesA, (128, 255, 128))
+    B = PolygonEntity(400, 400, verticesB, (128, 128, 255))
 
 firstFrameTriggered = False
 angle = 0
 def UpdateGame():
-    global firstFrameTriggered, B, angle
+    global firstFrameTriggered, A, B, angle
     if not firstFrameTriggered:
         BeginGame()
         firstFrameTriggered = True
 
     p = Point(100, 0)
-    if Screen.KeyDown(pygame.K_SPACE):
-        angle += 0.02
+    #if Screen.KeyDown(pygame.K_SPACE):
+    angle -= 0.05
     p.radians = angle
-    B.x = 50 + p.x
-    B.y = 20 + p.y
+
+    midPt = Point()
+    for pt in A.vertices:
+        midPt += pt
+    midPt /= len(A.vertices)
+    for pt in A.vertices:
+        pt.rotateRadians(0.03, midPt)
+    B.x = 300 + p.x
+    B.y = 400 + p.y
 
 def RenderGame():
     global A, B
     A.IntersectionPolygon(B, (255, 255, 0))
+    #input("Waiting")
     #B.IntersectionPolygon(A, (255, 0, 0))
 
 def StartGame():
-    Screen(640, 480)
+    Screen(800, 800)
     PreGame()
     Screen.Instance.AddUpdateFunction("main", UpdateGame)
     Screen.Instance.AddRenderFunction("main", RenderGame)
