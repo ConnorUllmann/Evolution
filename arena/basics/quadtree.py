939ab2cd31d5231815bc4e14dfe2c7650d4340e4
ConnorUllmann <c.ullmann@hotmail.com>
2017-03-16 01:34:47 -0400

    Added ability to query a rectangle or any objects colliding with an object that is already in the tree
    Also added the ability to insert a point into the quadtree rather than a
    rectangle (no modifications needed; simply pass in zero width and height
    for the rectangle)

--- arena/basics/quadtree.py
+++ arena/basics/quadtree.py
@@ -13,11 +13,21 @@ class QuadTree(Point):
         self.minCellHeight = minCellHeight
         self.root = QuadTreeNode(0, 0, width, height, self)
 
-    def insertObjectWithBoundingBox(self, x, y, width, height, obj):
-        self.root.insertObjectWithBoundingBox(x, y, width, height, obj)
+    def insertObjectAtPoint(self, obj, x, y):
+        self.root.insertObjectWithBoundingBox(obj, x, y, 0, 0)
+
+    def insertObjectWithBoundingBox(self, obj, x, y, width, height):
+        self.root.insertObjectWithBoundingBox(obj, x, y, width, height)
+
+    def collidingLeafNodes(self, obj):
+        return self.root.findCollidingLeafNodes(obj)
+
+    def collidingObjects(self, x, y, width, height):
+        return self.root.findCollidingObjects(x, y, width, height)
 
     def render(self):
         self.root.render()
+
 
 class QuadTreeNode(Point):
 
@@ -28,6 +38,7 @@ class QuadTreeNode(Point):
         self.quadTree = quadTree
         self.childTrees = []
         self.objectData = []
+        self.color = Color.green
 
     def split(self):
         halfWidth = self.width / 2
@@ -55,20 +66,61 @@ class QuadTreeNode(Point):
                self.height > self.quadTree.minCellHeight and \
                len(self.childTrees) <= 0
 
-    def insertObjectWithBoundingBox(self, x, y, width, height, obj):
-        if obj not in self.objectData and RectanglesCollide(self.x, self.y, self.width, self.height, x, y, width, height):
-            self.objectData.append((x, y, width, height, obj))
+    def collideRect(self, x, y, width, height):
+        return RectanglesCollide(self.x, self.y, self.width, self.height, x, y, width, height)
+
+    def insertObjectWithBoundingBox(self, obj, x, y, width, height):
+        if obj not in self.objectData and self.collideRect(x, y, width, height):
+            self.objectData.append((obj, x, y, width, height))
             if self.hasSplit:
                 for child in self.childTrees:
-                    child.insertObjectWithBoundingBox(x, y, width, height, obj)
+                    child.insertObjectWithBoundingBox(obj, x, y, width, height)
             elif self.shouldSplit and self.canSplit:
                 self.split()
                 for objectData in self.objectData:
                     for child in self.childTrees:
                         child.insertObjectWithBoundingBox(*objectData)
 
+    def findCollidingObjects(self, x, y, width, height):
+        collidingObjects = []
+        potentialCollidingObjectDatas = self.findPotentialCollidingObjects(x, y, width, height)
+        for pcod in potentialCollidingObjectDatas:
+            if RectanglesCollide(x, y, width, height, pcod[1], pcod[2], pcod[3], pcod[4]):
+                collidingObjects.append(pcod[0])
+        return collidingObjects
+
+    def findPotentialCollidingObjects(self, x, y, width, height, objectDatas=[]):
+        if self.collideRect(x, y, width, height):
+            if self.hasSplit:
+                for child in self.childTrees:
+                    child.findPotentialCollidingObjects(x, y, width, height, objectDatas)
+            else:
+                for objectData in self.objectData:
+                    objectDatas.append(objectData)
+        return objectDatas
+
+    def findCollidingLeafNodes(self, objInTree, leafNodes=[]):
+        for child in self.childTrees:
+            for objectData in child.objectData:
+                if objectData[0] != objInTree:
+                    continue
+                if child.hasSplit:
+                    child.findCollidingLeafNodes(objInTree, leafNodes)
+                else:
+                    leafNodes.append(child)
+                break
+        return leafNodes
+
+    def findPotentialCollidingObjectsWithObjectInTree(self, objInTree):
+        nodes = self.findCollidingLeafNodes(objInTree)
+        objects = []
+        for node in nodes:
+            for objectData in node.objectData:
+                objects.append(objectData[0])
+        return objects
+
     def render(self):
-        Screen.DrawRect(self, Point(self.width, self.height), Color.green, filled=False)
+        Screen.DrawRect(self, Point(self.width, self.height), self.color, filled=False)
         Screen.DrawText(self + Point(self.width/2, self.height/2), "{}".format(len(self.objectData)))
         for child in self.childTrees:
             child.render()
--- arena/main_quadtree.py
+++ arena/main_quadtree.py
@@ -3,26 +3,46 @@ from random import random
 
 rects = []
 quadtree = None
-
+leafNodes = []
+debugRectInfo = None
+debugRect = [Point(200, 300), Point(200, 150)]
 
 def Begin():
-    global rects, quadtree
+    global rects, quadtree, debugRectInfo
     for i in range(100):
-        rects.append([Point(Screen.Width() * random(), Screen.Height() * random()), Point(30, 30)])
-
+        width = max(80 * random(), 10)
+        height = max(80 * random(), 10)
+        x = Screen.Width() * random() - width / 2
+        y = Screen.Height() * random() - height / 2
+        rects.append([Point(x, y), Point(width, height), Color.light_red])
+    debugRectInfo = rects[47]
+    debugRectInfo[2] = Color.yellow
 
 def Update():
-    global rects, quadtree
+    global rects, quadtree, leafNodes, debugRectInfo
     quadtree = QuadTree(0, 0, Screen.Width(), Screen.Height())
     for rect in rects:
-        quadtree.insertObjectWithBoundingBox(rect[0].x, rect[0].y, rect[1].x, rect[1].y, rect)
-    #quadtree.printContentLengths()
+        quadtree.insertObjectWithBoundingBox(rect, rect[0].x, rect[0].y, rect[1].x, rect[1].y)
+
+    collidingObjects = quadtree.collidingObjects(debugRect[0].x, debugRect[0].y, debugRect[1].x, debugRect[1].y)
+    for collidingObject in collidingObjects:
+        collidingObject[2] = Color.light_blue
+
+    leafNodes = quadtree.collidingLeafNodes(debugRectInfo)
+    for leafNode in leafNodes:
+        leafNode.color = Color.blue
 
 def Render():
-    global rects, quadtree
+    global rects, quadtree, leafNodes, debugRect
     for rect in rects:
-        Screen.DrawRect(rect[0], rect[1], Color.light_blue)
-    quadtree.render()
+        Screen.DrawRect(rect[0], rect[1], rect[2])
+
+    Screen.DrawRect(debugRectInfo[0], debugRectInfo[1], debugRectInfo[2])
+    Screen.DrawRect(debugRect[0], debugRect[1], Color.cyan, 3, filled=False)
+
+    #quadtree.render()
+    for node in leafNodes:
+        node.render()
 
 if __name__ == '__main__':
     Screen.StartGame(Begin, Update, Render)
