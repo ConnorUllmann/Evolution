2e85b9f65baef509f274926a68e2c59dddea019a
ConnorUllmann <c.ullmann@hotmail.com>
2017-03-19 01:15:04 -0400

    Add performance measurement against brute force collisions between polygons

--- arena/main_quadtree_polygons.py
+++ arena/main_quadtree_polygons.py
@@ -1,6 +1,7 @@
 from basics import Screen, Point, Color, QuadTree, Polygon, PolygonEntity
 from random import random
 from math import pi
+from time import time
 
 polygons = []
 quadtree = None
@@ -9,8 +10,8 @@ def GeneratePolygon(x, y):
     vertices = []
     angle = 0.0
     while angle < 2 * pi:
-        length = random() * 15 + 5
-        angle = min(angle + random() * 0.6 + 0.3, 2*pi)
+        length = random() * 4 + 1
+        angle = min(angle + random() * 0.2 + 0.1, 2*pi)
         point = Point(length, 0)
         point.radians = angle
         vertices.append(point)
@@ -18,7 +19,7 @@ def GeneratePolygon(x, y):
 
 def Begin():
     global quadtree, polygons
-    for i in range(100):
+    for i in range(1000):
         polygon = PolygonEntity(GeneratePolygon(random() * Screen.Width(), random() * Screen.Height()), Color.light_blue)
         polygons.append(polygon)
 
@@ -31,6 +32,7 @@ def Update():
         minY = polygon.minY
         quadtree.insertObjectWithBoundingBox(polygon, minX, minY, polygon.maxX - minX, polygon.maxY - minY)
 
+    startTime = time()
     for polygon in polygons:
         minX = polygon.minX
         minY = polygon.minY
@@ -39,6 +41,16 @@ def Update():
         polygonsCollided = list(filter(lambda x: polygon.collide(x), polygonsCollided))
         if len(polygonsCollided) > 0:
             polygon.color = Color.red
+    print("Quad tree time:   {}s".format(time() - startTime))
+
+    startTime = time()
+    for polygonA in polygons:
+        for polygonB in polygons:
+            if polygonA == polygonB:
+                continue
+            if polygonA.collide(polygonB):
+                polygonA.color = polygonB.color = Color.red
+    print("Brute force time: {}".format(time() - startTime))
 
 def Render():
     global quadtree, polygons
@@ -54,4 +66,4 @@ def Render():
     Screen.DrawRect(position, size, Color.white, filled=False)
 
 if __name__ == '__main__':
-    Screen.StartGame(Begin, Update, Render)
+    Screen.StartGame(Begin, Update, Render, width=1000, height=750)
